38e1e73caa2383a7817afc8c3d38a52295b5a7ac
d.bystrikov <zelezodoroznik@yandex.ru>
2019-04-26 14:11:08 +0300

    новые тесты по отчётам для преыдущего месяца с полным отчётом по юзерам

---
+++ test/reports/common-games/User/test_previous_month_full_report_user.py
@@ -0,0 +1,16 @@
+# Отчёт за день + без галочки Кассовый отчёт + Обычные + Пользователь + предыдущий месяц, к примеру будет 10 число
+
+
+
+def test_previous_month_full_report_user(app):
+    app.report.open_page_report()
+    app.report.select_user()
+    app.report.previous_month_date_10()
+    app.report.without_checkbox_cash_report()
+    app.report.button_get_report()
+    app.report.parser_full_report_text()
+    assert "ОТЧЕТ ЗА ДЕНЬ" in app.report.parser_full_report_text()
+    assert "ИТОГИ ПО ПОЛЬЗОВАТЕЛЮ" in app.report.parser_full_report_text()
+    assert "Продавец: 2000006809-0020003510" in app.report.parser_full_report_text()
+    assert " Пользователь :0020003510" in app.report.parser_full_report_text()
+    app.report.comeback_main_page()
