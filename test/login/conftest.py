8fea3d1cfa0fbd3fddd4fdc20fdd40421bd55695
d.bystrikov <zelezodoroznik@yandex.ru>
2019-05-20 16:12:33 +0300

    2 новых теста + интеграция с дженкинс

--- test/login/conftest.py
+++ test/login/conftest.py
@@ -2,8 +2,18 @@ import pytest
 from fixture.application import Application
 
 
+fixture = None
+
+
 @pytest.fixture(scope="session")
 def app2(request):
-    fixture = Application()
+    global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    if fixture is None:
+        fixture = Application(browser=browser, base_url=base_url)
+    else:
+        if not fixture.is_valid():
+            fixture = Application(browser=browser, base_url=base_url)
     request.addfinalizer(fixture.destroy)
     return fixture
